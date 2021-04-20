c59e650b8139cf4da0c4fd67274e5176bd197bb1
Brenthubbard <76464105+Brenthubbard@users.noreply.github.com>
2021-04-20 12:44:22 -0600

    Fahrenheit coverter added
    Co-authored-by: Seth Medeiros <Medeirosseth@gmail.com>

--- src/main.js
+++ src/main.js
@@ -19,9 +19,18 @@ import './css/styles.css'
     };
     request.open("GET", url, true);
     request.send();
-
-    function getElements(response) {
-      $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
-      $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp} degrees.`);
-    }
+    
+        function getElements(response) {
+          const k = response.main.temp
+          const Fahrenheit = Math.floor((k-273.15)*9/5+32)
+    
+    $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
+    $('.showTemp').text(`The temperature in Kelvin is ${response.main.temp} degrees.`);
+    $('.showFahrenheit').text(`The temperature in Fahrenheit is ${Fahrenheit} degrees.`);
+    // $('.showClouds').text(`The current cloud level in ${city} is ${response.clouds}`);
+    // $('.showWind').text(`The current cloud level in ${city} is ${response.wind.speed.gust}`);
+  } 
+    
+    
+    
   });
