9dad6714320a6211008ddb9d4818d409689c248f
Brenthubbard <76464105+Brenthubbard@users.noreply.github.com>
2021-04-20 16:39:48 -0600

    add dry code
    Co-authored-by: Seth Medeiros <Medeirosseth@gmail.com>

--- src/main.js
+++ src/main.js
@@ -2,38 +2,33 @@ import $ from 'jquery'
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './css/styles.css'
+import WeatherService from './weather-service.js'
 
-
-$('#weatherLocation').click(function () {
-  const city = $('#location').val();
+function clearFields() {
   $('#location').val("");
-
-  let promise = new Promise(function (resolve, reject) {
-    let request = new XMLHttpRequest();
-    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`;
-
-    request.onload = function () {
-      if (this.status === 200) {
-        resolve(request.response);
-      } else {
-        reject(request.response)
-      };
-    };
-    request.open("GET", url, true);
-    request.send();
-  });
-
+  $('.showErrors').text("");
+  $('.showHumidity').text("");
+  $('.showTemp').text("");
+}
+$('#weatherLocation').click(function () {
+  let city = $('#location').val();
+  clearFields();
+  let promise = WeatherService.getWeather(city);
   promise.then(function (response) {
     const body = JSON.parse(response);
-    const k = response.main.temp
+    const k = body.main.temp
     const Fahrenheit = Math.floor((k - 273.15) * 9 / 5 + 32)
-
-    $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
-    $('.showTemp').text(`The temperature in Kelvin is ${response.main.temp} degrees.`);
+    $('.showHumidity').text(`The humidity in ${city} is ${body.main.humidity}%`);
+    $('.showTemp').text(`The temperature in Kelvin is ${body.main.temp} degrees.`);
     $('.showFahrenheit').text(`The temperature in Fahrenheit is ${Fahrenheit} degrees.`);
-    $('.showClouds').text(`The current cloud level in ${city} is ${response.clouds.all}`);
-    $('.showWind').text(`The current Wind speed in ${city} is ${response.wind.speed}`);
-
+    $('.showClouds').text(`The current cloud level in ${city} is ${body.clouds.all}`);
+    $('.showWind').text(`The current Wind speed in ${city} is ${body.wind.speed}`);
+    $('.showErrors').text("");
+  }, function(error) {
+    $('.showErrors').text(`There was an error processing your request:${error}`);
+    $('.showHumidity').text("");
+    $('.showTemp').text("");
   });
-  
 });
+    
+  
